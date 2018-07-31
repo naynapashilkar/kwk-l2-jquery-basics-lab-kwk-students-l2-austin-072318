555400b02a07109f05aa9c881c51897575c4bcc1
naynapashilkar <nayna.pashilkar@gmail.com>
2018-07-31 16:38:18 +0000

    automatically backed up by learn

--- js/calc.js
+++ js/calc.js
@@ -1,1 +1,24 @@
 //add your code here
+$(document).ready(function (){
+  doMath()
+})
+
+var num1 = $('#number1')
+var num2 = $('#number2')
+var operator = $('operation')
+
+function doMath() {
+  $('#equals').on('click',function () {
+  num1 = parseFloat(num1.val())
+  num2 = parseFloat(num2.val())
+  operator = operator.val()
+  result(num1,num2,operator)
+})
+    
+}
+
+function result (a,b,c) {
+ if (operator == "+") {
+   return $("#result").html(a+b)
+ }
+}
